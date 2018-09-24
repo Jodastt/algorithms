7b2e64469a1bc33179ab1a0a172bb46c22f0105f
Joseph S <38223151+Jodastt@users.noreply.github.com>
2018-09-23 18:12:41 -0700

    Create plusminus.js
    Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.

---
+++ plusminus.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the plusMinus function below.
+function plusMinus(arr) {
+    var pos = arr.filter(c => c > 0);
+    var neg = arr.filter(c => c < 0);
+    var zer = arr.filter(c => c == 0);
+    console.log((pos.length/arr.length).toFixed(6));
+    console.log((neg.length/arr.length).toFixed(6));
+    console.log((zer.length/arr.length).toFixed(6));
+};
+
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
